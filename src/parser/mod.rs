b41ac9d2cbe7ee5d210beaf4fc877a59ad5abb59
import wuzihao <1248951273@qq.com>
2023-04-30 08:47:59 +0000

    pratt parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -2,6 +2,7 @@ use rtor::{
     Parser,
     Input,
     AsChar,
+    Error,
     ParseResult,
     combine::{
         opt,
@@ -16,30 +17,105 @@ use rtor::{
 
 mod keyword;
 
-fn numeric_literal<I>(input: I) -> ParseResult<I, I> 
-    where I: Input<Token = char>
+#[derive(Debug)]
+pub enum LiteralValue {
+    Number(String)
+}
+
+#[derive(Debug)]
+pub enum BinOperator {
+    Plus,
+    Mul,
+}
+
+#[derive(Debug)]
+pub enum Expr {
+    LiteralValue(LiteralValue),
+    BinOp {
+        left: Box<Expr>,
+        op: BinOperator,
+        right: Box<Expr>
+    }
+}
+
+fn peek<I, P>(mut parser: P) -> impl Parser<I, Output = P::Output, Error = P::Error> 
+where 
+    I: Input,
+    P: Parser<I>
+{
+    move |input: I| {
+        let (o, _) = parser.parse(input.clone())?;
+        Ok((o, input))
+    }
+}
+
+fn unsigned_numeric_literal<I>(input: I) -> ParseResult<LiteralValue, I> 
+where I: Input<Token = char>
 {
     let src = input.clone();
 
     let fraction = '.'.and(skip_many(digit));
     let fraction1 = '.'.and(skip_many1(digit));
     let exponent = 'E'.or('e').and(opt('+'.or('-'))).and(skip_many1(digit));
-    let hexdigit = '0'.and('x'.or('X')).and(skip_many1(hex));
 
-    let (_, i) = hexdigit
-        .or(
-            skip_many1(digit)
-                .and(opt(fraction))
-                .or(fraction1)
-                .and(opt(exponent))
-        )
+    let (_, i) = skip_many1(digit)
+        .and(opt(fraction))
+        .or(fraction1)
+        .and(opt(exponent))
         .parse(input)?;
 
-    Ok((src.diff(&i),i))
+    let s = src.diff(&i).tokens().collect::<String>();
+
+    Ok((LiteralValue::Number(s), i))
 }
+
+fn binop<I>(input: I) -> ParseResult<(BinOperator, u8, u8), I> 
+where I: Input<Token = char>
+{
+    '+'.map(|_| (BinOperator::Plus, 1, 2))
+        .or('*'.map(|_| (BinOperator::Mul, 3, 4)))
+        .parse(input)
+}
+
+fn expr_binop<I>(min_bp: u8) -> impl Parser<I, Output = Expr, Error = Error<I::Token>>
+where I: Input<Token = char>
+{
+    move |mut input: I| {
+        let (value, i) = unsigned_numeric_literal.parse(input)?;
+
+        let mut left = Expr::LiteralValue(value);
+
+        input = i;
+
+        loop {
+            let ((op, l_bp, r_bp), i) = peek(binop).parse(input)?;
+
+            if l_bp < min_bp {
+                input = i;
+                break;
+            }
+
+            let (_, i) = binop.parse(i).unwrap();
+
+            let (right, i) = expr_binop(r_bp).parse(i)?;
+
+
+            left = Expr::BinOp { 
+                left: Box::new(left), 
+                op, 
+                right: Box::new(right)
+            };
+
+            input = i;
+        }
+
+        Ok((left, input))
+    }
+}
+
 
 #[test]
 fn test() {
-    let (s, _) = numeric_literal.parse("012e2").unwrap();
-    println!("{:?}", s.parse::<f32>())
+    let r =expr_binop(0).parse("1+2*3*4");
+    println!("{:?}", r)
 }
